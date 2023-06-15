1f7584ac593976d434042c9b03b02c4274485330
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-06-15 19:12:08 +0200

    [ADD] Pokemon Detail first style

--- pokedex/src/components/PokemonDetail.tsx
+++ pokedex/src/components/PokemonDetail.tsx
@@ -13,17 +13,22 @@ const PokemonDetail: React.FC = () => {
 
   const detailedPokemon = pokemon as PokemonData;
   return (
-    <div>
+    <div className="detail-container">
       <h1>{detailedPokemon.name}</h1>
       <img src={detailedPokemon.sprites?.other?.['official-artwork']?.front_default} alt={detailedPokemon.name} />
       <p>Height: {detailedPokemon.height}</p>
       <p>Weight: {detailedPokemon.weight}</p>
       <p>Abilities: {detailedPokemon.abilities?.map((ability) => ability.ability.name).join(', ')}</p>
       <p>Moves: {detailedPokemon.moves?.slice(0, 5).map((move) => move.move.name).join(', ')}</p>
-      <p>Types: {detailedPokemon.types?.map((type) => type.type.name).join(', ')}</p>
+      <div className="types">
+        {detailedPokemon.types?.map((type) => (
+          <span key={type.type.name} className="type-tag">{type.type.name}</span>
+        ))}
+      </div>
       <p>Stats: {detailedPokemon.stats?.map((stat) => `${stat.stat.name}: ${stat.base_stat}`).join(', ')}</p>
     </div>
   );
 };
+
 
 export default PokemonDetail;
