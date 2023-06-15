0ad70d83a3449d5c5b4a8a8145115f311f124bf8
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-06-15 21:33:50 +0200

    [ADD] search input using useState hook

--- pokedex/src/components/PokemonList.tsx
+++ pokedex/src/components/PokemonList.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import usePokemons from '../hooks/usePokemons';
 
 const PokemonList: React.FC = () => {
   const pokemons = usePokemons();
+  const [searchValue, setSearchValue] = useState('');
+
   if (!pokemons) {
     return <div>Loading...</div>;
   }
 
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
-    <div className="pokemon-grid">
-      {pokemons.map((pokemon, index) => (
-        <div key={index} className="pokemon-card">
-          <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
-        </div>
-      ))}
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Pokemon..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <div className="pokemon-grid">
+        {filteredPokemons.map((pokemon, index) => (
+          <div key={index} className="pokemon-card">
+            <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
