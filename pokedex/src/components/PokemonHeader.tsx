eff824b451fb455e2221f9b19416282191b1fa92
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-06-19 20:53:11 +0200

    [ADD] image loading state and spinner in PokemonHeader component

--- pokedex/src/components/PokemonHeader.tsx
+++ pokedex/src/components/PokemonHeader.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ClipLoader } from 'react-spinners';
 
 type PokemonHeaderProps = {
-    name: string;
-    image: string;
+  name: string;
+  image: string;
+};
+
+const PokemonHeader: React.FC<PokemonHeaderProps> = ({ name, image }) => {
+  const [isImageLoading, setImageLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setImageLoading(false);
   };
-  
-const PokemonHeader: React.FC<PokemonHeaderProps> = ({ name, image }) => (
-<div>
-    <h1>{name}</h1>
-    <img src={image} alt={name} />
-</div>
-);
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      {isImageLoading && <ClipLoader color="#000000" size={150} />}
+      <img src={image} alt={name} style={{display: isImageLoading ? 'none' : 'block'}} onLoad={handleImageLoad} />
+    </div>
+  );
+};
 
 export default PokemonHeader;
   
